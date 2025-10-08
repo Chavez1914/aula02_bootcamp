3f2ebc50640ad80931834b5784ee2ffd99b453ad
Andre Tomicioli <chavez.tomicioli@gmail.com>
2025-10-08 18:24:17 -0300

    Adicionado o exercicio_16

--- exercicios.py
+++ exercicios.py
@@ -95,14 +95,20 @@
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
-nome = input('Informe se primeiro nome: ')
-sobre_nome = input('Informe seu sobre nome: ')
-nome_completo = nome + sobre_nome
-print(f'Nome completo: {nome_completo}')
+#nome = input('Informe se primeiro nome: ')
+#sobre_nome = input('Informe seu sobre nome: ')
+#nome_completo = nome + sobre_nome
+#print(f'Nome completo: {nome_completo}')
 
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+valor1 = True
+valor2 = False
+resultado_and = valor1 and valor2
+print(f"Resultado do AND lógico:, {resultado_and}")
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
