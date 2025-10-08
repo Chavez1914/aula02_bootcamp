49df61c679d8c8caf9fedd9f2511c89d491fadd8
Andre Tomicioli <chavez.tomicioli@gmail.com>
2025-10-08 12:17:38 -0300

    Adicionado exercicio_03

--- exercicios.py
+++ exercicios.py
@@ -14,6 +14,12 @@ print(f'o resto da divisão de {n1} por 5 é: {resto_divisao}')
 
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe outro número: '))
+divisao = n1 * n2
+print(f'O resultado de {n1} x {n2} = {divisao} ')
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
