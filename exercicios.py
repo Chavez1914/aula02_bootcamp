46f63e221d37fea2daa133cc1b07fb108148c7ee
Andre Tomicioli <chavez.tomicioli@gmail.com>
2025-10-08 12:16:22 -0300

    Adicionado exercicio_02

--- exercicios.py
+++ exercicios.py
@@ -7,6 +7,12 @@ soma = n1 + n2
 print(f'A soma  de {n1} + {n2} é = {soma}')
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+n1 = int(input('Informe um número: '))
+resto_divisao = n1 % 5
+print(f'o resto da divisão de {n1} por 5 é: {resto_divisao}')
+
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
